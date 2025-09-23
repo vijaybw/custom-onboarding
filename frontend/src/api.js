8d7ac8d5be494b67db4750b9a772e7cb804464bd
Vijay Walunj <walunjvijay@gmail.com>
2025-09-23 00:13:48 -0400

    resolving issues with frontend baseURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://custom-onboarding-q00q.onrender.com/api/", // injected from vite.config.js
+  baseURL: import.meta.env.VITE_API_URL || "/api", // injected from vite.config.js
 });
 
 export default api;
